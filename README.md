1101eb2fe547d13c322268d2da6665ddff55bd40
gilsonroberto <gilsonroberto@gmail.com>
2020-10-26 12:23:06 -0300

    Commit Project Sky

---
+++ README.md
@@ -0,0 +1,47 @@
+# SKY – FrontEnd Test
+
+FrontEnd Teste Sky
+
+O desafio consite em desenvolver uma aplicação que exiba um catalogo, para isso será feito o consumo de Api.
+
+## Pré-requisitos
+
+ Para executar este projeto, você precisará ter um ambiente básico.
+ 
+-- Node >= 10.16
+
+-- Npm >= 6.9 
+ 
+ ----- Para verificar a versão instalada no seu computador do Node e Npm digite no terminal
+ 
+ $ node --version
+ 
+ $ npm --version
+
+
+## Instalando
+
+### Clonando o repositório
+
+$ git clone https://github.com/gilsonroberto/sky_FrontEndTest.git
+
+$ cd sky_FrontEndTest
+
+### Instalando dependências
+
+$ npm install
+
+(Irá instalar as dependências do projeto lendo o arquivo package.json)
+
+
+## Iniciando
+Com todas as dependências instaladas e o ambiente configurado corretamente, agora você pode executar a aplicação:
+
+$ npm start 
+
+(Script padrão para rodar em produção)
+
+A aplicação irá rodar no endereço: http://localhost:8080/
+
+
+<!-- ##Desenvolvido por Gilson Souza -->
---
+++ src/components/display/display.js
@@ -0,0 +1,61 @@
+import './display.css';
+
+const renderItems = items => {
+    let ret = '';
+    items.forEach(it => {
+        ret += `<div class="item">
+                    <img src="${it.images[0].url}" alt="${it.title}" title="${it.title}" />
+                </div>`;
+    });
+    return ret;
+}
+
+
+const Display = params => {
+    const section = document.createElement('section');
+    const next = `<div class="bt" nextDisplay="${params.id}"><i class="fas fa-chevron-right"></i></div>`;
+    const prev = `<div class="bt disabled" prevDisplay="${params.id}"><i class="fas fa-chevron-left"></i></div>`;
+
+    section.className = "categories";
+    section.setAttribute('displayId', params.id);
+    section.innerHTML = next + prev;
+    section.innerHTML += `
+                        <span>${params.title}</span>
+                        <div class="itens">
+                            ${renderItems(params.items)}
+                        </div>`;
+    document.querySelector("#displays").append(section);
+
+    const pixelsToScroll = 250;
+
+    const buttonRight = document.querySelector('[nextDisplay="' + params.id + '"]');
+    const buttonLeft = document.querySelector('[prevDisplay="' + params.id + '"]');
+    buttonRight.addEventListener('click', function () {
+        const element = document.querySelector('[displayId="'+this.getAttribute('nextDisplay')+'"] .itens');
+        const scroll = element.scrollLeft + pixelsToScroll;
+        const current = element.scrollLeft;
+        TweenMax.to(element, .3, {scrollLeft:scroll, ease:Circ.easeOut, onComplete:()=>{
+            if(element.scrollLeft > 0){
+                document.querySelector('[prevDisplay="' + this.getAttribute('nextDisplay') + '"]').classList.remove('disabled');
+            }
+            if(element.scrollLeft - current < pixelsToScroll){
+                this.classList.add('disabled');
+            }
+        }});
+    })
+    
+    buttonLeft.addEventListener('click', function () {
+        if(this.classList.contains('disabled')) return;
+        const element = document.querySelector('[displayId="'+this.getAttribute('prevDisplay')+'"] .itens');
+        const scroll = element.scrollLeft - pixelsToScroll;
+        TweenMax.to(element, .3, {scrollLeft:scroll, ease:Circ.easeOut, onComplete:()=>{
+            if(element.scrollLeft === 0){
+                this.classList.add('disabled');
+            }
+            document.querySelector('[nextDisplay="' + this.getAttribute('prevDisplay') + '"]').classList.remove('disabled');            
+        }});
+    })
+
+}
+
+export default Display;
